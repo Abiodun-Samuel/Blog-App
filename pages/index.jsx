24bb2f91569b63714f6122a1804676b7aa4f7260
Abiodun Samuel <abiodunsamyemi@gmail.com>
2022-04-05 05:32:46 +0100

    fix bug

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { PostCard, PostWidget, Categories } from './components'
 import { getPosts } from '../services'
+import { FeaturedPosts } from '../sections'
 
 const Home = ({ posts }) => {
   return (
@@ -9,12 +10,13 @@ const Home = ({ posts }) => {
         <title>My Blog App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post) => (
             <PostCard post={post.node} key={post.node.id} />
           ))}
-        </div> 
+        </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative top-8 lg:sticky">
             <PostWidget />
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -6,7 +6,7 @@ import {
   Categories,
   PostWidget,
   Author,
-  // Comments,
+  Comments,
   CommentsForm,
   //   Loader,
 } from '../components'
@@ -30,7 +30,7 @@ const PostDetails = ({ post }) => {
 
             {/* <AdjacentPosts slug={post.slug} createdAt={post.createdAt} /> */}
             <CommentsForm slug={post.slug} />
-            {/* <Comments slug={post.slug} /> */}
+            <Comments slug={post.slug} />
           </div>
           <div className="col-span-1 lg:col-span-4">
             <div className="relative top-8 lg:sticky">
